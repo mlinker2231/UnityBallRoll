7fd9fdf122041757bc984cd254605bd3bf025ffe
mlinker2231 <mlinker2231@stu.d214.org>
2021-08-25 13:32:55 -0500

    Initial Commit1
    Initial

---
+++ Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,18 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour
+{
+	public Transform player;
+    public TextMeshProUGUI scoreText;
+    private float count = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+        scoreText.text = (count).ToString("0");
+    }
+
+}

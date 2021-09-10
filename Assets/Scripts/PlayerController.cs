fb1a57e78753294f40ff2c902d2b1baab9a58d09
mlinker2231 <mlinker2231@stu.d214.org>
2021-09-10 13:40:58 -0500

    core

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce(movement * (speed));
     }
     void Update()
     {
--- Assets/Scripts/ScoreUpdate.cs
+++ Assets/Scripts/ScoreUpdate.cs
@@ -14,10 +14,35 @@ public class ScoreUpdate : MonoBehaviour
     {
         count = player.position.z;
         scoreText.text = (count).ToString("0");
-        if (player.position.z >= 600f)
+        if (player.position.z >= 600f && player.position.z <= 650)
         {
             winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 0f, 0f, 0f));
+            winText.text = "Level 1 Complete";
            
+           
+        }if (player.position.z >= 650 && player.position.z <= 700)
+        {
+            winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 90f, 0f, 0f));
+        }
+        if (player.position.z >= 1800 && player.position.z <= 1900)
+        {
+            winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 0f, 0f, 0f));
+            winText.text = "Level 2 Complete";
+
+        }
+        if (player.position.z >= 1950 && player.position.z <= 2000)
+        {
+            winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 90f, 0f, 0f));
+        }
+        if (player.position.z >= 3000 && player.position.z <= 3100)
+        {
+            winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 0f, 0f, 0f));
+            winText.text = "Final 3 Complete";
+
+        }
+        if (player.position.z >= 3150 && player.position.z <= 3200)
+        {
+            winText.transform.SetPositionAndRotation(winText.transform.position, new Quaternion(0f, 90f, 0f, 0f));
         }
     }
 

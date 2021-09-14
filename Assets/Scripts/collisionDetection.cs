119f21f64036829d062d3112153e39561fbef74d
mlinker2231 <mlinker2231@stu.d214.org>
2021-09-14 13:33:53 -0500

    hello
    gorls

--- Assets/Scripts/collisionDetection.cs
+++ Assets/Scripts/collisionDetection.cs
@@ -13,25 +13,27 @@ public class collisionDetection : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
 
-        if (collision.gameObject.name == "Start Terrain")
-        {
+        
+            print(collision.gameObject.tag);
+            print(collision);
 
             player.transform.SetPositionAndRotation(new Vector3(0, 0.5f, 0), new Quaternion());
             PlayerController playerScript = player.GetComponent<PlayerController>();
             playerScript.speed = 5;
-        }
+
+        
         if (collision.gameObject.name == "Level 2 Terrain")
         {
 
             player.transform.SetPositionAndRotation(new Vector3(0, 0.5f, 666), new Quaternion());
-            PlayerController playerScript = player.GetComponent<PlayerController>();
+            playerScript = player.GetComponent<PlayerController>();
             playerScript.speed = 5;
         }
         if (collision.gameObject.name == "Final Level Terrain")
         {
 
             player.transform.SetPositionAndRotation(new Vector3(0, 0.5f, 1800), new Quaternion());
-            PlayerController playerScript = player.GetComponent<PlayerController>();
+            playerScript = player.GetComponent<PlayerController>();
             playerScript.speed = 5;
         }
 
